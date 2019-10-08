9eff8517d0eaaba7d61b989750d5f18eb42150ea
DhruvilPatel <atdhruvilpatel@gmail.com>
2019-10-08 16:38:14 -0400

    init commit

---
+++ dhp68-sd997/01/multilaterate.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def multilaterate(distances):
+    return [0., 0., 0., 0.]
+
+
+if __name__ == "__main__":
+
+    # Retrive file name for input data
+    if len(sys.argv) == 1:
+        print("Please enter data file name.")
+        exit()
+
+    filename = sys.argv[1]
+
+    # Read data
+    lines = [line.rstrip('\n') for line in open(filename)]
+    distances = []
+    for line in range(0, len(lines)):
+        distances.append(list(map(float, lines[line].split(' '))))
+
+    # Print out the data
+    print("The input four points and distances, in the format of [x, y, z, d], are:")
+    for p in range(0, len(distances)):
+        print(*distances[p])
+
+        # Call the function and compute the location
+    location = multilaterate(distances)
+    print("The location of the point is: " + str(location))
